52e0115e3a1e3c894d82dc30534f60602882e5a4
Yarko Tymciurak <yarkot1@gmail.com>
2017-04-29 03:07:52 -0500

    section 9.2

---
+++ code/9/holding9_2.py
@@ -0,0 +1,32 @@
+# holding.py
+
+class Holding(object):
+    def __init__(self, name, date, shares, price):
+        self.name = name
+        self.date = date
+        self.shares = shares
+        self.price = price
+
+    def __repr__(self):
+        return 'Holding({!r},{!r},{!r},{!r})'.format(self.name, self.date, self.shares, self.price)
+
+    def __str__(self):
+        return '{} shares of {} at ${:0.2f}'.format(self.shares, self.name, self.price)
+
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, nshares):
+        self.shares -= nshares
+
+import csv
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename, 'r') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            h = Holding(row[0], row[1], int(row[2]), float(row[3]))
+            portfolio.append(h)
+    return portfolio
