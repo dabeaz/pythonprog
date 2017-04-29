51d9a2a5bf43f27a281bd8bb2af27ae208c948f9
Yarko Tymciurak <yarkot1@gmail.com>
2017-04-29 03:19:11 -0500

    9.3; update 9.2 to include __main__;

--- code/9/holding9_3.py
+++ code/9/holding9_3.py
@@ -21,6 +21,40 @@ class Holding(object):
 
 import csv
 
+class Portfolio(object):
+    def __init__(self):
+        self.holdings = []      
+
+    def __getattr__(self, name):
+        return getattr(self.holdings, name)
+
+    @classmethod
+    def from_csv(cls, filename):
+        self = cls()
+        with open(filename, 'r') as f:
+            rows = csv.reader(f)
+            headers = next(rows)
+            for row in rows:
+                h = Holding(row[0], row[1], int(row[2]), float(row[3]))
+                self.holdings.append(h)
+        return self
+
+    def total_cost(self):
+        return sum([h.shares * h.price for h in self.holdings])
+
+    def __len__(self):
+        return len(self.holdings)
+
+    def __getitem__(self, n):
+        if isinstance(n, str):
+            return [ h for h in self.holdings if h.name == n ]
+        else:
+            return self.holdings[n]
+
+    def __iter__(self):
+        return self.holdings.__iter__()
+
+
 def read_portfolio(filename):
     portfolio = []
     with open(filename, 'r') as f:
