82f78920ad91514c226f75a732e4c6390ca62e88
Yarko Tymciurak <yarkot1@gmail.com>
2017-04-29 01:08:41 -0500

    setup 7.1

---
+++ code/7/holding7_1.py
@@ -0,0 +1,26 @@
+# holding.py
+
+class Holding(object):
+    def __init__(self, name, date, shares, price):
+        self.name = name
+        self.date = date
+        self.shares = shares
+        self.price = price
+
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, nshares):
+        self.shares -= nshares
+
+import csv
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename, 'r') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            h = Holding(row[0], row[1], int(row[2]), float(row[3]))
+            portfolio.append(h)
+    return portfolio
