c11e842feacebdd82b24b4439326f8512c896ce1
Yarko Tymciurak <yarkot1@gmail.com>
2017-05-05 14:21:04 -0500

    WIP: need to check/cleanup holding10_{2,3}.py for errors

---
+++ code/10/validate10_3.py
@@ -0,0 +1,39 @@
+# validate.py
+
+class Typed(object):
+    expected_type = object
+
+    def __init__(self, name=None):
+        self.name = name
+
+    def __get__(self, instance, cls):
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        if not isinstance(value, self.expected_type):
+            raise TypeError('Expected {}'.format(self.expected_type))
+        instance.__dict__[self.name] = value
+
+class Integer(Typed):
+    expected_type = int
+
+class Float(Typed):
+    expected_type = float
+
+class String(Typed):
+    expected_type = str
+
+class Holding(object):
+    name = String('name')
+    shares = Integer('shares')
+    price = Float('price')
+    def __init__(self, name, date, shares, price):
+        self.name = name
+        self.date = date
+        self.shares = shares
+        self.price = price
+
+    @property
+    def cost(self):
+        return self.shares * self.price
+
