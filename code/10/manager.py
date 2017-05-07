fce03d84b590234111df0ad2ab93fbf1048a8f56
Yarko Tymciurak <yarkot1@gmail.com>
2017-05-07 15:25:30 -0500

    finish up lesson 10;

--- code/10/manager.py
+++
@@ -1,9 +0,0 @@
-class Manager(object):
-    def __enter__(self):
-        print('Entering')
-        return 'some value'
-
-    def __exit__(self, ty, val, tb):
-        print('Exiting')
-        print(ty, val, tb)
-
