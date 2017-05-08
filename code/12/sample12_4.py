a8d521499b4422f351998ff55eba98c73a3d4b6e
Yarko Tymciurak <yarkot1@gmail.com>
2017-05-07 23:12:16 -0500

    finish lesson 12

---
+++ code/12/sample12_4.py
@@ -0,0 +1,34 @@
+# sample.py
+
+from logcall12_4 import logformat, logmethods
+
+logged = logformat('YOU ARE CALLING {func.__name__}')
+
+@logged
+def add(x, y):
+    '''
+    Adds x and y
+    '''
+    return x + y
+
+@logged
+def sub(x, y):
+    return x - y
+
+@logged
+def mul(x, y):
+    return x * y
+
+
+@logmethods
+class Spam(object):
+    def __init__(self, value):
+        self.value = value
+
+    def yow(self):
+        print('Yow!')
+
+    def grok(self):
+        print('Grok!')
+
+    
